a9602beb2c8bb63025e49706309d8cabf7fc581e
ardin14 <2025bit245@std.must.ac.ug>
2026-08-23 11:27:43 +0300

    Update 106_fix_employee_number_unique_constraint.sql

--- neon-migrations/106_fix_employee_number_unique_constraint.sql
+++ neon-migrations/106_fix_employee_number_unique_constraint.sql
@@ -1,2 +1,10 @@
 ALTER TABLE employees DROP CONSTRAINT IF EXISTS employees_employee_number_key;
-ALTER TABLE employees ADD CONSTRAINT employees_employee_number_company_unique UNIQUE (company_id, employee_number);
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_constraint WHERE conname = 'employees_employee_number_company_unique'
+  ) THEN
+    ALTER TABLE employees ADD CONSTRAINT employees_employee_number_company_unique UNIQUE (company_id, employee_number);
+  END IF;
+END $$;
